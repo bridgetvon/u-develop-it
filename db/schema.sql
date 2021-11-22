b413b146877f330cb4a07912bd009eb0697142dc
bridgetvon <88166194+bridgetvon@users.noreply.github.com>
2021-11-22 14:41:11 -0700

    candidates table created with edits

---
+++ db/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE candidates (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    indsutry_connected BOOLEAN NOT NULL
+);
+
