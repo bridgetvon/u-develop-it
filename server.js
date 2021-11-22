ddb70c2e62ca927493ac2d7c7c5600cc69278206
bridgetvon <88166194+bridgetvon@users.noreply.github.com>
2021-11-22 16:29:20 -0700

    testing server connection with errors

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const PORT = process.envPORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 
 //express middleware
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
+
+//connect to database 
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //sql username
+        user: 'user',
+        //sql password 
+        password: 'bridgetcodes',
+        port: 3001,
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 
 
