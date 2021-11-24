de80360eccbbeacb3a7c555cc571284432e16a9c
bridgetvon <88166194+bridgetvon@users.noreply.github.com>
2021-11-23 21:49:06 -0700

    voter routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const inputCheck = require('../../utils/inputCheck');
 const router = express.Router();
+const db = require('../../db/connection');
 
 router.use(require('./candidateRoutes'));
 router.use(require('./partyRoutes'));
+router.use(require('./voterRoutes'));
 
 
 module.exports = router;
