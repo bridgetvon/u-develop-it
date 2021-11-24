feb36887e6294a3e5046a404921d526a48b4ec99
bridgetvon <88166194+bridgetvon@users.noreply.github.com>
2021-11-23 19:50:07 -0700

    join candidates and aprties tables

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,12 +1,18 @@
-INSERT INTO candidates (first_name, last_name, indsutry_connected)
+INSERT INTO parties (name, description)
+VALUES
+  ('JS Juggernauts', 'Eat breathe and sleep javascript'),
+  ('Heroes of HTML', 'HTML in seconds'),
+  ('Git gurus', 'Git masters');
+
+INSERT INTO candidates (first_name, last_name, party_id, indsutry_connected)
 VALUES 
-  ('Ronald', 'Firbank', 1),
-  ('Virginia', 'Woolf', 1),
-  ('Piers', 'Gaveston', 0),
-  ('Charles', 'LeRoi', 1),
-  ('Katherine', 'Mansfield', 1),
-  ('Dora', 'Carrington', 0),
-  ('Edward', 'Bellamy', 0),
-  ('Montague', 'Summers', 1),
-  ('Octavia', 'Butler', 1),
-  ('Unica', 'Zurn', 1);
+  ('Ronald', 'Firbank', 1, 1),
+  ('Virginia', 'Woolf', 1, 1),
+  ('Piers', 'Gaveston', 1, 0),
+  ('Charles', 'LeRoi', 2, 1),
+  ('Katherine', 'Mansfield', 2, 1),
+  ('Dora', 'Carrington', 3, 0),
+  ('Edward', 'Bellamy', 3, 0),
+  ('Montague', 'Summers', 3, 1),
+  ('Octavia', 'Butler', 3, 1),
+  ('Unica', 'Zurn', NULL, 1);
