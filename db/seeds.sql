ca42d0d79afa3c4121bb5e33ae79e4d70f2f1973
bridgetvon <88166194+bridgetvon@users.noreply.github.com>
2021-11-24 08:44:48 -0700

    votes tables and seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,22 +1,3 @@
-INSERT INTO parties (name, description)
-VALUES
-  ('JS Juggernauts', 'Eat breathe and sleep javascript'),
-  ('Heroes of HTML', 'HTML in seconds'),
-  ('Git gurus', 'Git masters');
-
-INSERT INTO candidates (first_name, last_name, party_id, indsutry_connected)
-VALUES 
-  ('Ronald', 'Firbank', 1, 1),
-  ('Virginia', 'Woolf', 1, 1),
-  ('Piers', 'Gaveston', 1, 0),
-  ('Charles', 'LeRoi', 2, 1),
-  ('Katherine', 'Mansfield', 2, 1),
-  ('Dora', 'Carrington', 3, 0),
-  ('Edward', 'Bellamy', 3, 0),
-  ('Montague', 'Summers', 3, 1),
-  ('Octavia', 'Butler', 3, 1),
-  ('Unica', 'Zurn', NULL, 1);
-
 INSERT INTO voters (first_name, last_name, email)
 VALUES
   ('James', 'Fraser', 'jf@goldenbough.edu'),
@@ -69,3 +50,23 @@ VALUES
   ('Hubert', 'Crackanthorpe', 'hcrackan@goodletters.com'),
   ('William', 'Carleton', 'wcarleton@literature.com'),
   ('Gerald', 'Griffin', 'ggriff@lit.net');
+
+INSERT INTO parties (name, description)
+VALUES
+  ('JS Juggernauts', 'Eat breathe and sleep javascript'),
+  ('Heroes of HTML', 'HTML in seconds'),
+  ('Git gurus', 'Git masters');
+
+INSERT INTO candidates (first_name, last_name, party_id, indsutry_connected)
+VALUES 
+  ('Ronald', 'Firbank', 1, 1),
+  ('Virginia', 'Woolf', 1, 1),
+  ('Piers', 'Gaveston', 1, 0),
+  ('Charles', 'LeRoi', 2, 1),
+  ('Katherine', 'Mansfield', 2, 1),
+  ('Dora', 'Carrington', 3, 0),
+  ('Edward', 'Bellamy', 3, 0),
+  ('Montague', 'Summers', 3, 1),
+  ('Octavia', 'Butler', 3, 1),
+  ('Unica', 'Zurn', NULL, 1);
+
